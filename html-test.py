bd2424865171aa2898709ada41f0dce345d39fc3
Sakthi  Senda <u6051686@anu.edu.au>
2020-07-08 18:16:14 +1000

    Code now creates css file, imported new font as well

--- html-test.py
+++ html-test.py
@@ -1,14 +1,48 @@
 import webbrowser
 import os
-print("current path " + os.path.dirname(os.path.abspath(__file__)))
+path = os.path.dirname(os.path.abspath(__file__))
+print("current path " + path)
 
-f = open('helloworld.html', 'w')
+fontSize = input("Font??")
+
+f = open('home.html', 'w')
+
+style = open('style.css', 'w')
 
 message = """<html>
-<head></head>
-<body><p>Hello World!</p></body>
+<head><link rel="stylesheet" type="text/css" href="style.css"></head>
+<body><p>Hello World! Sakthi </p></body>
 </html>"""
+
+
+stye_code = """
+@font-face {
+font-family: "Quicksand";
+src: url(quicksand/Quicksand_Dash.otf) format("opentype");
+}
+
+*{
+    size: A4;
+}
+
+body p{
+    font-family: Quicksand;
+    color: black;
+    font-size: 50px;"""
+
+
+            
+            
+
 
 f.write(message)
 f.close()
 
+style.write(stye_code)
+style.close()
+
+# filename = path + "/" + "h.html"
+filename = path + "/" + "h.html"
+webbrowser.open_new_tab("http://localhost:63343/worksheet-gen/home.html?_ijt=rllnffpfdq8do1g4nss6h2p6da")
+
+# webbrowser.open('https://www.google.com/')
